ea1605e31b8e64a6921501a7e772fdabe84e326c
Nick McDowall <nicholasinengland@hotmail.com>
2012-07-01 21:43:37 +0100

    Adding some grep examples

--- src/main/groovy/xml/XmlExamples.groovy
+++ src/main/groovy/xml/XmlExamples.groovy
@@ -1,27 +1,28 @@
-def xml = '''
-<shops>
-	<shop name='Algos' location='London'>
-		<stock>
-			<apples>4000</apples>
-			<pears>3200</pears>
-		</stock>
-	</shop>
-	<shop name='Brada' location='Rye'>
-		<stock>
-			<apples>137</apples>
-			<pineapple>3200</pineapple>
-		</stock>
-	</shop>
-	<shop name='Cando' location='Rye'>
-		<stock>
-			<apples>200</apples>
-			<oranges>3200</oranges>
-		</stock>
-	</shop>
-</shops>
+    def xml = '''
+    <shops>
+        <shop name='Algos' location='London'>
+            <stock>
+                <apples>4000</apples>
+                <pears>3200</pears>
+            </stock>
+        </shop>
+        <shop name='Brada' location='Rye'>
+            <stock>
+                <apples>137</apples>
+                <pineapple>3200</pineapple>
+            </stock>
+        </shop>
+        <shop name='Cando' location='Rye'>
+            <stock>
+                <apples>200</apples>
+                <oranges>3200</oranges>
+            </stock>
+        </shop>
+    </shops>
 '''
 
 def shops = new XmlSlurper().parseText(xml)
+
 
 
 // Find first match
@@ -33,16 +34,20 @@ assert 2 == shops.shop.findAll { it.@location == 'Rye' }.size()
 // Total count of all apples
 assert 4337 ==  shops.shop.stock.apples.collect{it.toInteger()}.sum()
 
-// Another way to count the number of apples
-assert 4337 == shops.'**'.findAll {it.name() == "apples"}.collect{it.toInteger()}.sum()
+// Another way to count the number of apples 
+assert 4337 == shops.'**'.grep {it.name() == "apples"}.sum{it.toInteger()}
 
 // Count the number of apples in Rye
 assert 337 == shops.shop.findAll { it.@location == 'Rye' }.collect{it.stock.apples.toInteger()}.sum()
+
+// Count the number of apples in Rye
+assert 337 == shops.shop.grep { it.@location == 'Rye' }.stock.apples.sum{it.toInteger()}
 
 // Get unique locations
 assert ['London', 'Rye'] == shops.shop.@location.collect{it.text()}.unique()
 
 // Find all the names of shops that sell oranges
 assert ['Cando'] == shops.shop.findAll{ it.'**'.findAll{it.name() == 'oranges'} }.collect{it.@name.text()}
+
 
 
