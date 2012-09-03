1b24a531fa1e7fdba2acd858d6dd0f1d83bcb7a2
Nick McDowall <nicholasinengland@hotmail.com>
2012-09-03 22:53:03 +0100

    fixing a spacing issue

--- SpockExamples/src/test/java/parameterised/GroovyParameterisedTest.groovy
+++ SpockExamples/src/test/java/parameterised/GroovyParameterisedTest.groovy
@@ -32,6 +32,6 @@ class GroovyParameterisedTest extends Specification {
 		    "a1"          | false
 		    "1a"          | false
 		    "aa"          | false
-			""            | false
+		    ""            | false
 	}
 }
